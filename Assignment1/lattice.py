d4d804a73bd3cfeb2d50a04dce4e1827238aaaa7
Adam Tonderski <atonderski@gmail.com>
2017-11-13 10:51:11 +0100

    Finish up code for assignment 1
    Add some results for assignment 1

--- Assignment1/lattice.py
+++ Assignment1/lattice.py
@@ -1,7 +1,5 @@
-import random
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Lattice(object):
@@ -13,33 +11,50 @@ class Lattice(object):
         self.diffusion_rate = diffusion_rate
 
         self.num_individuals = num_total_individuals
-        self.num_infected = num_infected
+        self.num_initially_infected = num_infected
+
+        self.num_infected = 0
+        self.num_susceptible = 0
+        self.num_recovered = 0
+
+        self.lattice = self._get_clear_lattice()
+
+        self.reset()
+
+    def reset(self):
+        self.num_infected = self.num_initially_infected
         self.num_susceptible = self.num_individuals - self.num_infected
         self.num_recovered = 0
 
         self.lattice = self._get_clear_lattice()
-        self.fig, self.ax = plt.subplots()
 
-    def initialize_individuals(self):
         for i in range(self.num_individuals):
-            x = random.randint(0, self.length-1)
-            y = random.randint(0, self.length-1)
+            x = np.random.randint(0, self.length)
+            y = np.random.randint(0, self.length)
             if i < self.num_infected:
                 self.lattice[x, y, 1] = 1
             else:
-                self.lattice[x,y,0] += 1
+                self.lattice[x, y, 0] += 1
 
-    def run_simulation(self, plot_during, plot_final=True):
+    def run_simulation(self, optimize=True, plot_during=False,
+                       plot_final=False):
         i = 0
         susceptibles = []
         infected = []
         recovered = []
-        while self.num_infected:
 
-            self.random_walk()
+        if plot_during:
+            fig1, ax1 = plt.subplots()
+
+        while self.num_infected:
+            self.random_walk(optimize=optimize)
             self.check_infection()
-            if plot_during and i % 10 == 0:
-                self.plot()
+            if plot_during and i % 20 == 0:
+                self.plot(ax1)
+            if i % 10000 == 0 and i != 0:
+                print('        simulation has run %s time steps' % i)
+                print('        infected: %s, recovered: %s' %
+                      (self.num_infected, self.num_recovered))
             susceptibles.append(self.num_susceptible)
             infected.append(self.num_infected)
             recovered.append(self.num_recovered)
@@ -51,58 +66,78 @@ class Lattice(object):
             ax.plot(recovered, 'g')
             ax.plot(infected, 'r')
             plt.show()
-        else:
-            print(self.num_recovered/self.num_individuals)
+        return self.num_recovered / self.num_individuals
 
-    def plot(self):
-        self.ax.clear()
-        self.ax.axis([0, self.length-1, 0, self.length-1])
-        susceptible_inds = np.nonzero(self.lattice[:,:,0])
-        self.ax.scatter(susceptible_inds[0], susceptible_inds[1], c='b')
-        infected_inds = np.nonzero(self.lattice[:,:,1])
-        self.ax.scatter(infected_inds[0], infected_inds[1], c='r')
-        recovered_inds = np.nonzero(self.lattice[:,:,2])
-        self.ax.scatter(recovered_inds[0], recovered_inds[1], c='g')
+    def plot(self, ax):
+        ax.clear()
+        ax.axis([0, self.length - 1, 0, self.length - 1])
+        susceptible_inds = np.nonzero(self.lattice[:, :, 0])
+        ax.scatter(susceptible_inds[0], susceptible_inds[1], c='b')
+        infected_inds = np.nonzero(self.lattice[:, :, 1])
+        ax.scatter(infected_inds[0], infected_inds[1], c='r')
+        recovered_inds = np.nonzero(self.lattice[:, :, 2])
+        ax.scatter(recovered_inds[0], recovered_inds[1], c='g')
         plt.pause(0.01)
 
-    def random_walk(self):
+    def random_walk(self, optimize):
         new_lattice = self._get_clear_lattice()
-        for x in range(self.length):
-            for y in range(self.length):
-                for _ in range(self.lattice[x,y,0]):
-                    new_x, new_y = self._get_next_position(x,y)
-                    new_lattice[new_x,new_y,0] += 1
-                for _ in range(self.lattice[x,y,1]):
-                    new_x, new_y = self._get_next_position(x,y)
-                    new_lattice[new_x,new_y,1] += 1
-                for _ in range(self.lattice[x, y, 2]):
+
+        if optimize:
+            new_lattice[:, :, 2] = self.lattice[:, :, 2]
+            nonzero_indices = np.nonzero(np.sum(self.lattice[:, :, :2], 2))
+        else:
+            nonzero_indices = np.nonzero(np.sum(self.lattice, 2))
+
+        for x, y in zip(nonzero_indices[0], nonzero_indices[1]):
+
+            for _ in range(self.lattice[x, y, 0]):
+                if np.random.rand() < self.diffusion_rate:
                     new_x, new_y = self._get_next_position(x, y)
+                else:
+                    new_x, new_y = x, y
+                new_lattice[new_x, new_y, 0] += 1
+
+            for _ in range(self.lattice[x, y, 1]):
+                if np.random.rand() < self.diffusion_rate:
+                    new_x, new_y = self._get_next_position(x, y)
+                else:
+                    new_x, new_y = x, y
+                new_lattice[new_x, new_y, 1] += 1
+
+            if not optimize:
+                for _ in range(self.lattice[x, y, 2]):
+                    if np.random.rand() < self.diffusion_rate:
+                        new_x, new_y = self._get_next_position(x, y)
+                    else:
+                        new_x, new_y = x, y
                     new_lattice[new_x, new_y, 2] += 1
+
         self.lattice = new_lattice
 
     def check_infection(self):
-        for x in range(self.length):
-            for y in range(self.length):
-                num_infected = self.lattice[x,y,1]
-                for i in range(num_infected):
-                    if random.random() < self.beta:
-                        # Infect all susceptible individuals at the site
-                        self.lattice[x,y,1] += self.lattice[x,y,0]
-                        self.num_infected += self.lattice[x,y,0]
-                        self.num_susceptible -= self.lattice[x,y,0]
-                        self.lattice[x,y,0] = 0
-                    if random.random() < self.gamma:
-                        # Recover (remove from lattice)
-                        self.lattice[x,y,1] -= 1
-                        self.lattice[x,y,2] += 1
-                        self.num_infected -= 1
-                        self.num_recovered += 1
+        nonzero_indices = np.nonzero(self.lattice[:, :, 1])
+        if len(nonzero_indices[0]) == 0:
+            assert self.num_infected == 0
+            return
+        for x, y in zip(nonzero_indices[0], nonzero_indices[1]):
+            num_infected = self.lattice[x, y, 1]
+            for i in range(num_infected):
+                if self.lattice[x, y, 0] and np.random.rand() < self.beta:
+                    # Infect all susceptible individuals at the site
+                    self.lattice[x, y, 1] += self.lattice[x, y, 0]
+                    self.num_infected += self.lattice[x, y, 0]
+                    self.num_susceptible -= self.lattice[x, y, 0]
+                    self.lattice[x, y, 0] = 0
+                if np.random.rand() < self.gamma:
+                    # Recover (remove from lattice)
+                    self.lattice[x, y, 1] -= 1
+                    self.lattice[x, y, 2] += 1
+                    self.num_infected -= 1
+                    self.num_recovered += 1
 
     def _get_next_position(self, x, y):
-        if random.random() > self.diffusion_rate:
-            return x, y
-        r = random.random()
-        if r < 1/4:
+        r = np.random.rand()
+        if r < 1 / 4:
             x -= 1
         if r < 0.5:
             x += 1
@@ -118,4 +153,4 @@ class Lattice(object):
         return x, y
 
     def _get_clear_lattice(self):
-        return np.zeros((self.length,self.length,3), dtype=np.int8)
+        return np.zeros((self.length, self.length, 3), dtype=np.int8)
--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,11 +1,104 @@
 import lattice
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib import cm
+# noinspection PyUnresolvedReferences
+from mpl_toolkits.mplot3d import Axes3D  # NOQA
 
 
-l = lattice.Lattice(length=100,
-                    num_total_individuals=1000,
-                    num_infected=10,
-                    beta=0.5,
-                    diffusion_rate=0.3,
-                    gamma=0.005)
-l.initialize_individuals()
-l.run_simulation(plot_during=False, plot_final=False)
+def visualize_simple_walk():
+    l = lattice.Lattice(length=10,
+                        num_total_individuals=1,
+                        num_infected=1,
+                        beta=0.5,
+                        diffusion_rate=0.5,
+                        gamma=0)
+    l.run_simulation(plot_during=True, plot_final=False)
+
+
+def detailed_plot_of_single_run():
+    l = lattice.Lattice(length=100,
+                        num_total_individuals=1000,
+                        num_infected=10,
+                        beta=0.5,
+                        diffusion_rate=0.5,
+                        gamma=0.005)
+    l.run_simulation(plot_during=True, plot_final=True)
+
+
+def _run_simulation(beta, r_0, diff_rate, iterations=5):
+    gamma = np.float64(beta) / r_0
+    print("running with beta: %s, r_0: %s, gamma: %s" % (beta, r_0, gamma))
+    l = lattice.Lattice(length=100,
+                        num_total_individuals=1000,
+                        num_infected=10,
+                        beta=beta,
+                        diffusion_rate=diff_rate,
+                        gamma=gamma)
+    r_inf = 0
+    for i in range(iterations):
+        print("    iteration %s..." % i)
+        l.reset()
+        tmp_r_inf = l.run_simulation(plot_during=False, plot_final=False)
+        print("        %s" % tmp_r_inf)
+        r_inf += tmp_r_inf
+    avg_r_inf = r_inf / iterations
+    print("    average: %s" % avg_r_inf)
+    return avg_r_inf
+
+
+def plot_epidemic_thresholds(beta):
+    diff_rate = 0.3
+    r_0s = []
+    r_infs = []
+    for r_0 in np.linspace(0, 200, num=20):
+        print("running with beta: %s and r_0: %s" % (beta, r_0))
+        r_inf = _run_simulation(beta, r_0, diff_rate, iterations=5)
+        print(r_inf)
+        r_0s.append(r_0)
+        r_infs.append(r_inf)
+    print(r_0s, r_infs)
+    plt.plot(r_0s, r_infs)
+    plt.xlabel(r'$R_0$')
+    plt.ylabel(r'$R_{\inf}$')
+    plt.title(r'd=%s $\beta$=%s' % (diff_rate, beta))
+    plt.show()
+
+
+def plot_surface(beta_resolution, r0_resolution, iterations):
+    beta_resolution *= 1j
+    beta_resolution *= 1j
+    BETA, R_0 = np.mgrid[0.01:1:beta_resolution, 0.01:250:r0_resolution]
+    BETA = BETA.T
+    R_0 = R_0.T
+
+    print(R_0)
+    print(BETA)
+
+    vectorized_func = np.vectorize(_run_simulation, otypes=[np.float])
+
+    R_INF = vectorized_func(BETA, R_0, 0.3, iterations=iterations)
+
+    print(R_INF)
+
+    # Plot the surface.
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(BETA, R_0, R_INF, cmap=cm.gray,
+                           linewidth=0, antialiased=False)
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_xlabel(r'$\beta$')
+    ax.set_ylabel(r'$R_0$')
+    ax.set_zlabel(r'$R_{\inf}$')
+    ax.set_title(r'$\beta$-$R_0$-plane')
+
+    plt.savefig('beta_r0_plane.pdf')
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_epidemic_thresholds(beta=0.8)
+    #    detailed_plot_of_single_run()
+    # plot_surface(beta_resolution=10,r0_resolution=20, iterations=2)
